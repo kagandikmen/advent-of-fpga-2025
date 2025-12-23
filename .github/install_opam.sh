48461d4243ff7254e75af39750b188aab9921cd6
Kagan Dikmen <kagandikmen@outlook.com>
2025-12-23 09:05:51 +0100

    Fix CI #5

--- .github/install_opam.sh
+++ .github/install_opam.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-apt-get install opam
+apt-get install opam -y
 opam init -y
-eval $(opam env)
-opam install -y ocaml-lsp-server odoc ocamlformat utop
 opam switch create hardcaml 4.13.1
-opam repo add janestreet-bleeding https://ocaml.janestreet.com/opam-repository
-opam repo add janestreet-bleeding-external https://github.com/janestreet/opam-repository.git#external-packages
+eval $(opam env --switch=hardcaml)
+opam install -y ocaml-lsp-server odoc ocamlformat utop
+opam repo add janestreet-bleeding https://ocaml.janestreet.com/opam-repository -y
+opam repo add janestreet-bleeding-external https://github.com/janestreet/opam-repository.git#external-packages -y
 opam install -y hardcaml hardcaml_waveterm ppx_jane ppx_expect ppx_deriving_hardcaml
