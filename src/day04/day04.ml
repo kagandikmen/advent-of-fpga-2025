8f4570b3b3dd1b29f7e2bd0bb2213bda7385cb49
Kagan Dikmen <kagandikmen@outlook.com>
2026-01-11 12:46:50 +0100

    Add new Hardcaml solution for day 4 puzzle

--- src/day04/day04.ml
+++ src/day04/day04.ml
@@ -1,8 +1,8 @@
 (*
  *
- * AoF - Hardcaml Solution for Day 4 (Step 1 & Step 2)
+ * AoF - Hardcaml Solution for Day 4 (NEW)
  * Created:     2025-12-22
- * Modified:    2025-12-28
+ * Modified:    2026-01-11
  * Author:      Kagan Dikmen
  *
  *)
@@ -15,6 +15,7 @@ open! Hardcaml_aof
 open! Hardcaml_arty
 open! Signal
 
+let u4 x = uresize x 4
 let mul140 x = (sll x 7) +: (sll x 3) +: (sll x 2)
 let add_offset addr off = if off >= 0 then addr +:. off else addr -:. (-off)
 
@@ -22,16 +23,16 @@ module Cell = struct
   type 'a t =
   {
     is_roll: 'a;
-    is_accessible: 'a;
+    removed_this_pass: 'a;
   }
 
   let pack (x: Signal.t t) : Signal.t = 
-    concat_msb [ x.is_accessible; x.is_roll; ]
+    concat_msb [ x.removed_this_pass; x.is_roll; ]
 
   let unpack (x: Signal.t) : Signal.t t =
     {
       is_roll = select x 0 0;
-      is_accessible = select x 1 1;
+      removed_this_pass = select x 1 1;
     }
 end
 
@@ -68,9 +69,6 @@ let create_day04_logic ~clock ~clear ~cycles_per_bit ~max_passes uart_rx_value =
     uart_rx_value
   in
 
-  let _uart_value = uart_rx.value -- "uart_value" in
-  let _uart_valid = uart_rx.valid -- "uart_valid" in
-
   let is_at = (uart_rx.valid &: (uart_rx.value ==:. Char.to_int '@')) -- "is_at" in
   let is_dot = (uart_rx.valid &: (uart_rx.value ==:. Char.to_int '.')) -- "is_dot" in
   let is_cell = (is_at |: is_dot) -- "is_cell" in
@@ -85,7 +83,7 @@ let create_day04_logic ~clock ~clear ~cycles_per_bit ~max_passes uart_rx_value =
   let phase_w = wire 2 in 
   let phase = reg spec phase_w -- "phase" in
   let p_load = phase ==:. 0 in
-  let p_mark = phase ==:. 1 in
+  let p_switch = phase ==:. 1 in  (* takes one cycle *)
   let p_remove = phase ==:. 2 in
   let p_done = phase ==:. 3 in
 
@@ -102,13 +100,13 @@ let create_day04_logic ~clock ~clear ~cycles_per_bit ~max_passes uart_rx_value =
   let last_col = col ==:. dim in
   let last_cell = (last_row &: last_col) -- "last_cell" in
 
-  let step_en = ((p_load &: is_cell) |: p_mark |: p_remove) -- "step_en" in 
+  let step_en = ((p_load &: is_cell) |: p_remove) -- "step_en" in 
 
   let p_load_done = (p_load &: is_cell &: last_cell) -- "p_load_done" in
-  let p_mark_done = (p_mark &: last_cell) -- "p_mark_done" in
+  let p_switch_done = p_switch in
   let p_remove_done = (p_remove &: last_cell) -- "p_remove_done" in
 
-  (* important: remove_ctr shows how many removes are COMPLETED *)
+  (* important: remove_ctr shows how many remove passes are COMPLETED *)
   let remove_ctr = reg_fb spec
     ~enable:vdd
     ~width:8
@@ -117,20 +115,20 @@ let create_day04_logic ~clock ~clear ~cycles_per_bit ~max_passes uart_rx_value =
     -- "remove_ctr"
   in
 
-  (* nr of removable cells found in mark state *)
-  let latest_pass_removed_ctr_w = wire 16 in
-  let latest_pass_removed_ctr = reg spec latest_pass_removed_ctr_w -- "latest_pass_removed_ctr" in
-  let latest_pass_removed_zero = (latest_pass_removed_ctr ==:. 0) -- "latest_pass_removed_zero" in
+  (* nr of removable cells found in remove state *)
+  let pass_removed_ctr_w = wire 16 in
+  let pass_removed_ctr = reg spec pass_removed_ctr_w -- "pass_removed_ctr" in
+  let pass_removed_zero = (pass_removed_ctr ==:. 0) -- "pass_removed_zero" in
 
-  let is_next_remove = p_mark_done in
-  let is_next_done = p_remove_done &: (((max_passes ==:. 0) &: latest_pass_removed_zero) |: ((max_passes >:. 0) &: (max_passes ==: (remove_ctr +: one 8)))) in
-  let is_next_mark_from_load = p_load_done in
-  let is_next_mark_from_remove = p_remove_done &: (~:is_next_done) in
-  let is_next_mark = is_next_mark_from_load |: is_next_mark_from_remove in
+  let is_next_remove = p_switch_done in
+  let is_next_done = p_remove_done &: (((max_passes ==:. 0) &: pass_removed_zero) |: ((max_passes >:. 0) &: (max_passes ==: (remove_ctr +: one 8)))) in
+  let is_next_switch_from_load = p_load_done in
+  let is_next_switch_from_remove = p_remove_done &: (~:is_next_done) in
+  let is_next_switch = is_next_switch_from_load |: is_next_switch_from_remove in
 
   let phase_next =
     mux2 clear (zero 2)
-      (mux2 is_next_mark (one 2)
+      (mux2 is_next_switch (one 2)
         (mux2 is_next_remove (Signal.of_int ~width:2 2)
           (mux2 is_next_done (Signal.of_int ~width:2 3)
               phase)))
@@ -195,36 +193,51 @@ let create_day04_logic ~clock ~clear ~cycles_per_bit ~max_passes uart_rx_value =
   in
 
   let current_cell = Cell.unpack cell_mem.(0) in
+  let nw_cell = Cell.unpack cell_mem.(1) in
+  let n_cell = Cell.unpack cell_mem.(2) in
+  let ne_cell = Cell.unpack cell_mem.(3) in
+  let e_cell = Cell.unpack cell_mem.(4) in
+
   let is_roll_current_cell = current_cell.is_roll -- "is_roll_current_cell" in
-  let is_accessible_current_cell = current_cell.is_accessible -- "is_accessible_current_cell" in
+  let nw_cell_removed = nw_cell.removed_this_pass in
+  let n_cell_removed = n_cell.removed_this_pass in
+  let ne_cell_removed = ne_cell.removed_this_pass in
+  let e_cell_removed = e_cell.removed_this_pass in
   
   let nb_cells = Array.init 8 ~f:(fun i -> Cell.unpack cell_mem.(i+1)) in
-  let num_nb_rolls = Array.fold nb_cells ~init:(zero 4) ~f:(fun acc c -> acc +: uresize c.is_roll 4) -- "num_nb_rolls" in
-
-  let remove_en = (p_remove &: is_roll_current_cell &: is_accessible_current_cell) -- "remove_en" in
-
-  let load_we = (p_load &: is_cell) -- "load_we" in
-  let load_wdata = (Cell.pack { is_roll = is_at; is_accessible = zero 1; }) -- "load_wdata" in
-
-  let mark_we = (num_nb_rolls <:. 4) -- "mark_we" in
-  let mark_wdata = (Cell.pack { is_roll = is_roll_current_cell; is_accessible = (num_nb_rolls <:. 4); }) -- "mark_wdata" in
-  let remove_wdata = Cell.pack { is_roll = gnd; is_accessible = zero 1; } in 
-  (* does not matter that p_remove zeroes out is_accessible, it is recomputed during the following p_mark *)
-
-  assign cell_we_w (mux2 p_load load_we (mux2 p_mark mark_we (mux2 p_remove remove_en gnd)));
-  assign cell_wdata_w (mux2 p_load load_wdata (mux2 p_mark mark_wdata (mux2 p_remove remove_wdata (zero 2))));
-
-  let latest_pass_removed_ctr_next =
-    mux2 clear
-      (zero 16)
-      (mux2 is_next_mark
-        (zero 16)
-        (mux2 (p_remove &: remove_en)
-          (latest_pass_removed_ctr +:. 1)
-          latest_pass_removed_ctr))
+  let num_nb_rolls_raw = Array.fold nb_cells ~init:(zero 4) ~f:(fun acc c -> acc +: uresize c.is_roll 4) -- "num_nb_rolls" in
+  let num_nb_rolls =
+    (num_nb_rolls_raw 
+      +: u4 (nw_cell_removed ==:. 1)
+      +: u4 (n_cell_removed ==:. 1)
+      +: u4 (ne_cell_removed ==:. 1)
+      +: u4 (e_cell_removed ==:. 1))
+    -- "num_nb_rolls" 
   in
 
-  assign latest_pass_removed_ctr_w latest_pass_removed_ctr_next;
+  let load_we = (p_load &: is_cell) -- "load_we" in
+  let load_wdata = (Cell.pack { is_roll = is_at; removed_this_pass = zero 1; }) -- "load_wdata" in
+
+  let is_accessible_current_cell = (num_nb_rolls <:. 4) -- "is_accessible_current_cell" in
+  let is_current_cell_removed_last_pass = current_cell.removed_this_pass in
+
+  let remove_en = p_remove &: ((is_roll_current_cell &: is_accessible_current_cell) |: is_current_cell_removed_last_pass) -- "remove_en" in
+  let remove_wdata = Cell.pack { is_roll = gnd; removed_this_pass = (is_roll_current_cell &: is_accessible_current_cell); } in 
+
+  assign cell_we_w (mux2 p_load load_we (mux2 p_remove remove_en gnd));
+  assign cell_wdata_w (mux2 p_load load_wdata (mux2 p_remove remove_wdata (zero 2)));
+
+  let pass_removed_ctr_next =
+    mux2 clear
+      (zero 16)
+      (mux2 is_next_switch
+        (zero 16)
+        (mux2 (p_remove &: remove_en)
+          (pass_removed_ctr +:. 1)
+          pass_removed_ctr))
+  in
+
+  assign pass_removed_ctr_w pass_removed_ctr_next;
 
   let total_removed = reg_fb spec
     ~enable:vdd
@@ -232,11 +245,23 @@ let create_day04_logic ~clock ~clear ~cycles_per_bit ~max_passes uart_rx_value =
     ~f:(fun prev -> 
       mux2 clear
         (zero 16)
-        (mux2 remove_en
+        (mux2 (remove_en &: (~:is_current_cell_removed_last_pass))
           (prev +:. 1)
           prev))
     -- "total_removed"
   in
 
-  total_removed, p_done
+  (* waveform stuff *)
+  let uart_value = uart_rx.value -- "uart_value" in
+  let uart_valid = uart_rx.valid -- "uart_valid" in
+
+  let debug_output =
+    concat_msb
+      [
+        uart_value;
+        uart_valid;
+      ];
+  in
+
+  total_removed, p_done, debug_output
 ;;
--- test/day04/test_day04.ml
+++ test/day04/test_day04.ml
@@ -2,7 +2,7 @@
  *
  * AoF - Testbench for the Solution of Day 4
  * Created:     2025-12-22
- * Modified:    2025-12-28
+ * Modified:    2026-01-11
  * Author:      Kagan Dikmen
  *
  *)
@@ -15,25 +15,19 @@ open! Signal
 
 module Waveform = Hardcaml_waveterm.Waveform
 
-let parse_line =
-  fun (s: string) -> 
-    let s = String.strip s in
-    if String.is_empty s then None
-    else Some s
-;;
-
 let day04_test ~max_passes =
   let cycles_per_bit = 4 in
 
-  let lines = In_channel.read_lines "input.txt"
-    |> List.filter_map ~f:parse_line
+  let stream = In_channel.read_all "input.txt"
+    |> String.to_list
+    |> List.map ~f:Char.to_int
   in
   
   let clock = input "clock" 1 in
   let clear = input "clear" 1 in
   let uart_rx_value = input "uart_rx_value" 1 in
 
-  let total_rolls_collected, is_done = Day04.create_day04_logic
+  let total_rolls_collected, is_done, debug_output = Day04.create_day04_logic
     ~clock
     ~clear
     ~cycles_per_bit
@@ -46,6 +40,7 @@ let day04_test ~max_passes =
     [
       output "total_rolls_collected" total_rolls_collected;
       output "is_done" is_done;
+      output "debug_output" debug_output;
     ]
   in
 
@@ -59,6 +54,7 @@ let day04_test ~max_passes =
   let clear_in = Cyclesim.in_port sim "clear" in
   let total_rolls_collected = Cyclesim.out_port ~clock_edge:Before sim "total_rolls_collected" in
   let is_done = Cyclesim.out_port ~clock_edge:Before sim "is_done" in
+  let _debug_out = Cyclesim.out_port ~clock_edge:Before sim "debug_output" in
 
   let sim_driver = Simulation.create
     ~sim
@@ -75,11 +71,14 @@ let day04_test ~max_passes =
   sim_driver.wait 5; 
   clear_in := Bits.gnd;
 
-  List.iter lines ~f:(fun line ->
-    String.iter line ~f:sim_driver.uart.send_ascii_char;
-  );
+  
+  sim_driver.uart.send_byte 0x02; (* ascii for start of text *)
 
-  sim_driver.wait_until_done ~step:1000 ~max_steps:20_000_000;
+  List.iter stream ~f:sim_driver.uart.send_byte;
+
+  sim_driver.uart.send_byte 0x03; (* ascii for end of text *)
+
+  sim_driver.wait_until_done ~step:50 ~max_steps:200_000;
 
   let final_trc = Bits.to_int !total_rolls_collected in
 
